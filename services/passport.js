b848b2d780f268216aad928f97b3ecf7f2622cf2
gopolar <7777kara@gmail.com>
2020-01-26 10:10:06 +0100

    sets authentication cookies

--- services/passport.js
+++ services/passport.js
@@ -5,18 +5,32 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passprt.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+
+passprt.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+            done(null, user);
+        })
+});
+
 passprt.use(
     new GoogleStrategy(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
-        }, (accessToken, refreshToken, profile, done) => {
+        },
+        (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleID: profile.id }).then((existingUser) => {
                 if (existingUser) {
-                    console.log('----->', existingUser);
+                    done(null, existingUser);
                 } else {
-                    new User({ googleID: profile.id }).save();
+                    new User({ googleID: profile.id })
+                        .save()
+                        .then(user => done(null, user));
                 }
             });
 
