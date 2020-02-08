8e293de3fceb582328d2a4467e149f81c748a2cd
gopolar <7777kara@gmail.com>
2020-02-08 19:06:04 +0100

    refactors promises to async/await

--- services/passport.js
+++ services/passport.js
@@ -12,8 +12,8 @@ passprt.serializeUser((user, done) => {
 
 passprt.deserializeUser((id, done) => {
     User.findById(id).then(user => {
-            done(null, user);
-        })
+        done(null, user);
+    })
 });
 
 passprt.use(
@@ -24,17 +24,13 @@ passprt.use(
             callbackURL: '/auth/google/callback',
             proxy: true,
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleID: profile.id }).then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({ googleID: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            });
-
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({googleID: profile.id});
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+            const user = await new User({googleID: profile.id}).save();
+            done(null, user);
         }
     )
 );
