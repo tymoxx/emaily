d82487fe1951298c3b61d8268e6e854734289179
gopolar <7777kara@gmail.com>
2020-02-09 09:42:59 +0100

    header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,14 @@ import React, {Component} from "react";
 class Header extends Component {
     render() {
         return(
-            <div>Header</div>
+            <nav className='nav-wrapper'>
+                <a className='left brand-logo'>Emaily</a>
+                <ul className='right'>
+                    <li>
+                        <a>Log in with Google</a>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
