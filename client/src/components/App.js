6cc97a839e6b9b4db0e68c07565aa93ca99d6d45
gopolar <7777kara@gmail.com>
2020-02-09 09:13:10 +0100

    router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => {return <h2>Header</h2> };
+const Dashbord = () => {return <h2>Dashbord</h2> };
+const ServeyNew = () => {return <h2>ServeyNew</h2> };
+const Landing = () => {return <h2>Landing</h2> };
 
 const App = () => {
     return (
         <div>
-            Hi there
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route exact path='/' component={Landing}/>
+                    <Route exact path='/surveys' component={Dashbord}/>
+                    <Route path='/surveys/new' component={ServeyNew}/>
+                </div>
+            </BrowserRouter>
         </div>
     )
 };
