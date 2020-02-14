e15922891bac4dd495aba87d63013ee80c2fab25
gopolar <7777kara@gmail.com>
2020-02-14 08:40:51 +0100

    refactors action from promises to async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,12 +3,10 @@ import {FETCH_USER} from './types';
 
 
 // action creators
-export const fetchUser = () => {
-    return function(dispatch) {
-        axios.get('/api/current_user')
-            .then(res => dispatch({
-                type: FETCH_USER,
-                payload: res
-            }))
-    }
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user');
+    dispatch({
+        type: FETCH_USER,
+        payload: res
+    })
 };
